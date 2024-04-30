9667c7935bb4762fdffcfa61cf00f2b8a1ca0ad9
Leonardo <leonardocastillo1410@gmail.com>
2024-04-29 22:26:48 -0400

    ejercicio2

--- ejercicios script/ejercicio2.js
+++ ejercicios script/ejercicio2.js
@@ -3,17 +3,14 @@
 ///a 18 y los ingresos son iguales o mayores a 1000 debe retornar ingresos * 40%. De lo contrario retornar 0.
 
 function calcularImpuestos(edad, ingresos) {
-    if (edad >= 18) {
-      if (ingresos >= 1000) {
-        return ingresos * 0.4;
-      }  else {
-        return 0;
-      }
+  if (edad >= 18 && ingresos >= 1000) {
+    return ingresos * 0.4;
   } else {
-        return 0;
-      }
+    return 0;
   }
-  
-  console.log(calcularImpuestos(19,2000));
-  console.log(calcularImpuestos(15,2222));
-  
+}
+
+console.log(calcularImpuestos(19, 2000), calcularImpuestosAntigua(19, 2000)); // e i => 0.4 * i
+console.log(calcularImpuestos(15, 2222), calcularImpuestosAntigua(15, 2222)); // !e i => 0
+console.log(calcularImpuestos(15, 2222), calcularImpuestosAntigua(19,50)); // e !i => 0
+console.log(calcularImpuestos(15, 2222), calcularImpuestosAntigua(15,2));
